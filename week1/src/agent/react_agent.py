932eca954059cd45f62fcb5a7b89c2290a5fd2fd
baiding72 <486534919@qq.com>
2025-12-29 01:54:07 +0800

    fix: 响应格式

--- week1/src/agent/react_agent.py
+++ week1/src/agent/react_agent.py
@@ -2,14 +2,13 @@ from langchain.agents import create_agent
 from src.llm.qwen import get_llm
 from src.tools.web_search import web_search
 
-def create_agent():
+def create_react_agent():
     llm = get_llm()
     tools = [web_search]
 
     agent = create_agent(
         model=llm,
-        tools=tools,
-        verbose=True
+        tools=tools
     )
 
     return agent
--- week1/src/llm/qwen.py
+++ week1/src/llm/qwen.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+import os
 
 load_dotenv()
 
 def get_llm():
     return ChatOpenAI(
         model="qwen-plus",
+        openai_api_key=os.getenv("QWEN_API_KEY"),
+        openai_api_base=os.getenv("QWEN_BASE_URL"),
         temperature=0
     )
